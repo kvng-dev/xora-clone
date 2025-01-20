21ebc8d18e33925ad44ffe13072dd465113d8c4c
kvng-dev <oghuanlan@gmail.com>
2025-01-20 23:20:54 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Features from "./pages/Features";
+import Header from "./pages/Header";
+import Hero from "./pages/Hero";
+
+function App() {
+  return (
+    <main className="overflow-hidden">
+      <Header />
+      <Hero />
+      <Features />
+    </main>
+  );
+}
+
+export default App;
